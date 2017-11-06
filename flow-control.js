bf7a59ad29ef95c2c1295f3f269776a0ecbcc0e5
Victoria <vitalina27@gmail.com>
2017-11-06 08:49:09 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -8,12 +8,12 @@ function teenager(age) {
   if(age>=13 && age<=19){
     return 'You are a teenager!';
   }else {
-    return "You are not a teenager!";
+    return "You are not a teenager";
   }
 }
 
 function ageChecker(age) {
-  if(age<=13){
+  if(age<13){
     return 'You are a kid';
   }
    else if(age>=13 && age<=19){
@@ -24,7 +24,7 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-return (age>=13 && age <=19)? "You are a teenager!":"You are not a teenager!"
+return (age>=13 && age <=19)? "You are a teenager!":"You are not a teenager"
 }
 
 function switchAge(age) {
