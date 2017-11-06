198449ffaaa259b4577e4c134698aa196738656a
Victoria <vitalina27@gmail.com>
2017-11-06 08:41:50 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -14,7 +14,7 @@ function teenager(age) {
 
 function ageChecker(age) {
   if(age<=13){
-    return 'You are kid';
+    return 'You are a kid';
   }
    else if(age>=13 && age<=19){
     return 'You are a teenager';
