98255651169d1c7c375e186d655f85587118eb75
Victoria <vitalina27@gmail.com>
2017-11-06 08:43:34 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -1,14 +1,14 @@
 function basicTeenager(age) {
  if(age>=13 && age<=19){
-   return 'You are a teenager';
+   return 'You are a teenager!';
  }
 }
 
 function teenager(age) {
   if(age>=13 && age<=19){
-    return 'You are a teenager';
+    return 'You are a teenager!';
   }else {
-    return "You are not a teenager";
+    return "You are not a teenager!";
   }
 }
 
@@ -17,14 +17,14 @@ function ageChecker(age) {
     return 'You are a kid';
   }
    else if(age>=13 && age<=19){
-    return 'You are a teenager';
+    return 'You are a teenager!';
   }else {
     return "You are a grownup";
   }
 }
 
 function ternaryTeenager(age) {
-return (age>=13 && age <=19)? "You are a teenager":"You are not a teenager"
+return (age>=13 && age <=19)? "You are a teenager!":"You are not a teenager!"
 }
 
 function switchAge(age) {
